541aed164a06e2cc6a7158087e6223539102436f
Deepak B <deepakb@trinesis.com>
2021-02-27 21:26:27 +0530

    update 27

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     MONGOURI: process.env.MOGOURI,
-    JWT_SECRET: process.env.JWT_SEC,
+    JWT_SECRET: process.env.JWT_SECRET,
     SENDGRID_API: process.env.SENDGRID_API,
     EMAIL: process.env.EMAIL,
   };
